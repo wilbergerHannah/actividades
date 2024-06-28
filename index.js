63204aa60e0d41d393b9710d224d8299ac94eed6
wilbergerHannah <166016835+wilbergerHannah@users.noreply.github.com>
2024-06-28 20:18:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//Actividad 1:
+
+
+for (let indice = 10 ; indice > 0 ; indice - 1)
+    console.log (indice)
+
+
+
+
+// Actividad 2:
+
+
+for (let indice = 15 ; indice >4 ; indice - 1)
+   console.log (indice)
+
+
+
+
+//Actividad 3:
+
+
+for (let indice = 1 ; indice >= 100 ; indice + 2 ) 
+     console.log (indice)
+
+
+//Actividad 4: 
+
+
+for (let indice = 0 ; indice <=70 ; indice + 7)
+    console.log (indice)
